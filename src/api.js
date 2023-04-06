73b03e9955dfe61242c57b51e7535ffaed450371
Smith Ringtho <sringtho@gmail.com>
2023-04-06 20:49:59 +0300

    Added axios to replace fetch for the questions api

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
-
+import axios from "axios"
 
 export async function getQuestions(id){
     const url = id ? `http://127.0.0.1:5000/questions/${id}` 
     : `http://127.0.0.1:5000/questions`
-    const res = await fetch(url)
-    const data = res.json()
-    return data
+    const data = await axios.get(url)
+    console.log(data)
+    return data.data
 }
 
 export async function postQuestion(formData, token){
